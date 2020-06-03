aefcb815e662f3230c6093026a1d717c59d84722
jossiebk <bkcastrillo@gmail.com>
2020-06-03 11:09:08 -0600

    interfaz grafica inicial y algunos metodos de interfaz

---
+++ ventana.py
@@ -0,0 +1,265 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+#from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+import accionesIDE as accionesVarias
+import mostrarLineas
+from random import seed
+from random import randint
+
+#clase central de mi ventana a mostrarse
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        c=accionesVarias.accionesIde()
+        MainWindow.setObjectName("MainWindow")
+        #tamaño que tendra la ventana 1400px x 900px
+        MainWindow.resize(1400, 900)
+        #le indico que se abra desde el centro de la pantalla pooniendolo en el objeto ventanaCentrada
+        self.ventanaCentrada = QtWidgets.QWidget(MainWindow)
+        self.ventanaCentrada.setObjectName("ventanaCentrada")
+        #se crea etiqueta
+        #self.EdicionTexto = QtWidgets.QPlainTextEdit(self.ventanaCentrada)
+        self.EdicionTexto = mostrarLineas.QCodeEditor(self.ventanaCentrada)
+        #para definir su posicion
+        self.EdicionTexto.setGeometry(QtCore.QRect(40, 70, 860, 500))
+        font = QtGui.QFont()
+        # para cambiar el tamaño de la fuente de un objeto
+        font.setPointSize(16)
+        self.EdicionTexto.setFont(font)
+        self.EdicionTexto.setObjectName("EdicionTexto")
+
+        #area del debugger
+        self.DebugTexto = QtWidgets.QPlainTextEdit(self.ventanaCentrada)
+        self.DebugTexto.setGeometry(QtCore.QRect(930, 70, 450, 500))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.DebugTexto.setFont(font)
+        self.DebugTexto.setObjectName("DebugTexto")
+
+        #area de consola de salida
+        self.ConsolaSalida = QtWidgets.QTextEdit(self.ventanaCentrada)
+        self.ConsolaSalida.setGeometry(QtCore.QRect(40, 590, 1340, 260))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.ConsolaSalida.setFont(font)
+        self.ConsolaSalida.setObjectName("ConsolaSalida")
+
+        MainWindow.setCentralWidget(self.ventanaCentrada)
+        #fijo la barra de menu en la parte superior
+        self.barraMenu = QtWidgets.QMenuBar(MainWindow)
+        self.barraMenu.setGeometry(QtCore.QRect(0, 0, 800, 36))
+        self.barraMenu.setObjectName("barraMenu")
+        #archivo
+        self.archivoMenu = QtWidgets.QMenu(self.barraMenu)
+        self.archivoMenu.setObjectName("archivoMenu")
+        #editar
+        self.editarMenu = QtWidgets.QMenu(self.barraMenu)
+        self.editarMenu.setObjectName("editarMenu")
+        #ejecutar
+        self.ejecutarMenu = QtWidgets.QMenu(self.barraMenu)
+        self.ejecutarMenu.setObjectName("ejecutarMenu")
+        #opciones
+        self.opcionesMenu = QtWidgets.QMenu(self.barraMenu)
+        self.opcionesMenu.setObjectName("opcionesMenu")
+        #ayuda
+        self.ayudaMenu = QtWidgets.QMenu(self.barraMenu)
+        self.ayudaMenu.setObjectName("ayudaMenu")
+        #------------------------------------------------
+        MainWindow.setMenuBar(self.barraMenu)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        #acciones de Archivo
+        self.accionNuevo = QtWidgets.QAction(MainWindow)
+        self.accionNuevo.setObjectName("accionNuevo")
+        self.accionAbrir = QtWidgets.QAction(MainWindow)
+        self.accionAbrir.setObjectName("accionAbrir")
+        self.accionGuardar = QtWidgets.QAction(MainWindow)
+        self.accionGuardar.setObjectName("accionGuardar")
+        self.accionGuardarComo = QtWidgets.QAction(MainWindow)
+        self.accionGuardarComo.setObjectName("accionGuardarComo")
+        self.accionCerrar = QtWidgets.QAction(MainWindow)
+        self.accionCerrar.setObjectName("accionCerrar")
+        self.accionSalir = QtWidgets.QAction(MainWindow)
+        self.accionSalir.setObjectName("accionSalir")
+        
+        self.archivoMenu.addAction(self.accionNuevo)
+        self.archivoMenu.addAction(self.accionAbrir)
+        self.archivoMenu.addAction(self.accionGuardar)
+        self.archivoMenu.addAction(self.accionGuardarComo)
+        self.archivoMenu.addAction(self.accionCerrar)
+        self.archivoMenu.addAction(self.accionSalir)
+        #acciones de editar
+        self.accionBuscar = QtWidgets.QAction(MainWindow)
+        self.accionBuscar.setObjectName("accionBuscar")
+        self.accionReemplazar = QtWidgets.QAction(MainWindow)
+        self.accionReemplazar.setObjectName("accionReemplazar")
+
+
+        self.editarMenu.addAction(self.accionBuscar)
+        self.editarMenu.addAction(self.accionReemplazar)
+        #acciones Ejecutar
+        self.accionDescendente = QtWidgets.QAction(MainWindow)
+        self.accionDescendente.setObjectName("accionDescendente")
+        self.accionAscendente = QtWidgets.QAction(MainWindow)
+        self.accionAscendente.setObjectName("accionAscendente")
+        self.accionPasos = QtWidgets.QAction(MainWindow)
+        self.accionPasos.setObjectName("accionPasos")
+        self.accionSimbolos = QtWidgets.QAction(MainWindow)
+        self.accionSimbolos.setObjectName("accionsimbolos")
+        self.accionAST = QtWidgets.QAction(MainWindow)
+        self.accionAST.setObjectName("accionAST")
+        self.accionGramatical = QtWidgets.QAction(MainWindow)
+        self.accionGramatical.setObjectName("accionGramatical")
+        self.accionErrores = QtWidgets.QAction(MainWindow)
+        self.accionErrores.setObjectName("accionErrores")
+
+        self.ejecutarMenu.addAction(self.accionDescendente)
+        self.ejecutarMenu.addAction(self.accionAscendente)
+        self.ejecutarMenu.addAction(self.accionPasos)
+        self.ejecutarMenu.addAction(self.accionSimbolos)
+        self.ejecutarMenu.addAction(self.accionAST)
+        self.ejecutarMenu.addAction(self.accionGramatical)
+        self.ejecutarMenu.addAction(self.accionErrores)
+        #acciones de opciones
+        self.accionColor = QtWidgets.QAction(MainWindow)
+        self.accionColor.setObjectName("accionColor")
+        self.accionLinea = QtWidgets.QAction(MainWindow)
+        self.accionLinea.setObjectName("accionLinea")
+
+        self.opcionesMenu.addAction(self.accionColor)
+        self.opcionesMenu.addAction(self.accionLinea)
+        #acciones de ayuda y acerca de
+        self.accionAyuda = QtWidgets.QAction(MainWindow)
+        self.accionAyuda.setObjectName("accionAyuda")
+        self.accionAcerca = QtWidgets.QAction(MainWindow)
+        self.accionAcerca.setObjectName("accionAcerca")
+
+        self.ayudaMenu.addAction(self.accionAyuda)
+        self.ayudaMenu.addAction(self.accionAcerca)
+        #genera los botones y su accion de despliegue
+        self.barraMenu.addAction(self.archivoMenu.menuAction())
+        self.barraMenu.addAction(self.editarMenu.menuAction())
+        self.barraMenu.addAction(self.ejecutarMenu.menuAction())
+        self.barraMenu.addAction(self.opcionesMenu.menuAction())
+        self.barraMenu.addAction(self.ayudaMenu.menuAction())
+        #acciones de los botones de cada menu
+        
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        #funciones de archivo
+        self.accionNuevo.triggered.connect(lambda: self.EdicionTexto.insertPlainText(""))
+        self.accionAbrir.triggered.connect(self.abrir_archivo)
+        self.accionGuardar.triggered.connect(lambda: self.guardar_normal(self.EdicionTexto.toPlainText()))
+        self.accionGuardarComo.triggered.connect(lambda: self.guardar_como(self.EdicionTexto.toPlainText()))
+        self.accionCerrar.triggered.connect(lambda: c.cerrarVentana(QtWidgets.QApplication(sys.argv)))
+        self.accionSalir.triggered.connect(lambda: c.cerrarVentana(QtWidgets.QApplication(sys.argv)))
+        #funciones de editar
+        #self.accionBuscar.triggered.connect(lambda: self.clicked(path))
+        #self.accionReemplazar.triggered.connect(lambda: self.clicked(path))
+        #funciones de ejecutar
+
+        #funciones de opciones
+        self.accionColor.triggered.connect(lambda: self.cambiarColor())
+        #funciones de ayuda
+        self.accionAcerca.triggered.connect(lambda: self.ventanaAcercaDe("hola"))
+
+    def retranslateUi(self, MainWindow):
+        #aca solo setteo los nombres en cada componente y un sub nombre por asi decirlo
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.EdicionTexto.insertPlainText(_translate("MainWindow", ""))
+        self.archivoMenu.setTitle(_translate("MainWindow", "Archivo"))
+        self.editarMenu.setTitle(_translate("MainWindow", "Editar"))
+        self.ejecutarMenu.setTitle(_translate("MainWindow", "Ejecutar"))
+        self.opcionesMenu.setTitle(_translate("MainWindow", "Opciones"))
+        self.ayudaMenu.setTitle(_translate("MainWindow", "Ayuda"))
+        #de editar
+        self.accionBuscar.setText(_translate("MainWindow", "Buscar"))
+        self.accionReemplazar.setText(_translate("MainWindow", "Reemplazar"))
+        # de archivo
+        self.accionNuevo.setText(_translate("MainWindow", "Nuevo"))
+        self.accionNuevo.setShortcut(_translate("MainWindow", "Ctrl+N"))
+        self.accionAbrir.setText(_translate("MainWindow", "Abrir"))
+        self.accionAbrir.setShortcut(_translate("MainWindow", "Ctrl+O"))
+        self.accionGuardar.setText(_translate("MainWindow", "Guardar"))
+        self.accionGuardar.setShortcut(_translate("MainWindow", "Ctrl+S"))
+        self.accionGuardarComo.setText(_translate("MainWindow", "Guardar Como"))
+        self.accionGuardarComo.setShortcut(_translate("MainWindow", "Ctrl+P"))
+        self.accionCerrar.setText(_translate("MainWindow", "Cerrar"))
+        self.accionCerrar.setShortcut(_translate("MainWindow", "Ctrl+W"))
+        self.accionSalir.setText(_translate("MainWindow", "Salir"))
+        self.accionSalir.setShortcut(_translate("MainWindow", "Ctrl+E"))
+        #de ejecutar
+        self.accionDescendente.setText(_translate("MainWindows","Analisis Descendente"))
+        self.accionAscendente.setText(_translate("MainWindows","Analisis Ascendente"))
+        self.accionPasos.setText(_translate("MainWindows","Analisis paso a paso"))
+        self.accionSimbolos.setText(_translate("MainWindows","Tabla de simbolos"))
+        self.accionAST.setText(_translate("MainWindows","AST"))
+        self.accionGramatical.setText(_translate("MainWindows","Reporte Gramatical"))
+        self.accionErrores.setText(_translate("MainWindows","Reporte de Errores"))
+        #de opciones
+        self.accionColor.setText(_translate("MainWindows","Cambiar color"))
+        self.accionLinea.setText(_translate("MainWindows","Cambiar Numeracion"))
+        #de ayuda
+        self.accionAyuda.setText(_translate("MainWindows","Ayuda"))
+        self.accionAcerca.setText(_translate("MainWindows","Acerca de"))
+
+#self.EdicionTexto.setText(c.abrirArchivo("C:\\Users\\Jossie Castrillo\\Desktop\\[OLC2]P1_201313692\\prueba.txt"))
+    def clicked(self, text):
+        self.EdicionTexto.insertPlainText(path)
+        print(path)
+        #self.EdicionTexto.adjustSize()
+    #metodo para cambiar el color del fondo del entorno de escritura unicamente (falta mejorar)
+    def cambiarColor(self):
+        valor=randint(0,10)
+        print(valor)
+        if valor %2 ==0:
+            self.EdicionTexto.setStyleSheet("background: white")
+        else:
+            self.EdicionTexto.setStyleSheet("background: grey")
+    #metodo para mostrar la ventana emergente con los datos del desarrollador, osea yo
+    def ventanaAcercaDe(self,t):
+        print(t)
+        msg=QtWidgets.QMessageBox()
+        msg.setWindowTitle("Acerca De...")
+        msg.setText("Jossie Bismarck Castrillo Fajardo - 201313692")
+        msg.exec_()
+    #metodo para ventana emergente generica
+    def ventanaEmergente(self,mensaje):
+        msg=QtWidgets.QMessageBox()
+        msg.setWindowTitle("Notificacion")
+        msg.setText(mensaje)
+        msg.exec_()
+    #este metodo genera el file chooser donde se podra buscar el archivo y devuelve la ruta del archivo
+    #posteriormente se manda la ruta el meetodo c.abrirArchivo del import accionesIDE y devuelve el texto abierto
+    def abrir_archivo(self):
+        c=accionesVarias.accionesIde()
+        dialog = QtWidgets.QFileDialog()
+        fname = dialog.getOpenFileName(None, "Abrir archivo", "", "all (*.*)")
+        global path
+        path=fname[0]
+        self.EdicionTexto.insertPlainText(c.abrirArchivo(path))
+        print(path)
+    #envia el texto del editor y la ruta de un archivo ya abierto al metodo del import accionesIDE de guardarArchivo
+    def guardar_como(self,texto):
+        c=accionesVarias.accionesIde()
+        name = QtWidgets.QFileDialog.getSaveFileName(None, 'Save File')
+        print(name[0])
+        if c.guardarArchivo(name[0],texto):
+            self.ventanaEmergente("Se ha guardado su archivo exitosamente!")
+    #abre un file chooser para seleccionar un lugar para guardar el archivo y permite escribir el nombre con su extension.
+    #esa ruta se envia con el texto del editor al metodo guardarArchivo
+    def guardar_normal(self,texto):
+        c=accionesVarias.accionesIde()
+        if c.guardarArchivo(path,texto):
+            self.ventanaEmergente("Se ha guardado su archivo exitosamente!")
+
+#inicializacion del main y su interfaz para ejecucion
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
